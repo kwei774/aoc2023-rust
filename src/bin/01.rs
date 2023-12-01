cafd11a4be439cbd758e19234d2815a575d7d5e4
Kevin <5399649+kwei774@users.noreply.github.com>
2023-11-30 22:40:27 -0500

    feat: 2022 parts 1/2 in rust

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,48 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(_input: &str) -> Option<u32> {
-    None
+    let v: Vec<&str> = _input.split('\n').collect();
+    let mut total_calories = 0;
+    let mut highest_cal = 0;
+    for element in v {
+        if element.parse::<u32>().is_ok(){
+            total_calories = total_calories + element.parse::<u32>().unwrap();
+        } else {
+            if highest_cal < total_calories {
+                highest_cal = total_calories
+            }
+            total_calories = 0
+        }
+    }
+    Some(highest_cal)
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
-    None
+    let v: Vec<&str> = _input.split('\n').collect();
+    let mut total_calories = 0;
+    let mut highest_calories = vec![0,0,0];
+    for element in v {
+        if element.parse::<u32>().is_ok(){
+            total_calories = total_calories + element.parse::<u32>().unwrap();
+        } else {
+            if highest_calories[0] < total_calories {
+                highest_calories[2] = highest_calories[1];
+                highest_calories[1] = highest_calories[0];
+                highest_calories[0] = total_calories;
+            } else if highest_calories[1] < total_calories {
+                highest_calories[2] = highest_calories[1];
+                highest_calories[1] = total_calories;
+            } else if highest_calories[2] < total_calories {
+                highest_calories[2] = total_calories;
+            }
+            total_calories = 0
+        }
+    }
+    let mut sum_highest_calories = 0;
+    for each in highest_calories {
+        sum_highest_calories += each
+    }
+    Some(sum_highest_calories)
 }
 
 pub fn first_function() -> i32 {
@@ -19,13 +56,13 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(24000));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(41000));
     }
 
     #[test]
