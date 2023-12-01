97ee222738051a9ff019278b3e3fa52e382b5f08
Kevin <5399649+kwei774@users.noreply.github.com>
2023-12-01 00:42:17 -0500

    feat: day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,52 +1,40 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(_input: &str) -> Option<u32> {
-    let v: Vec<&str> = _input.split('\n').collect();
-    let mut total_calories = 0;
-    let mut highest_cal = 0;
-    for element in v {
-        if element.parse::<u32>().is_ok(){
-            total_calories = total_calories + element.parse::<u32>().unwrap();
-        } else {
-            if highest_cal < total_calories {
-                highest_cal = total_calories
+    let mut sum = 0;
+    let lines: Vec<&str> = _input.split('\n').collect();
+    for line in lines{
+        let mut calibration = 0;
+        for character in line.chars() {
+            if character.is_numeric() {
+                calibration = character.to_digit(10).unwrap() * 10;
+                break;
             }
-            total_calories = 0
         }
+        for character in line.chars().rev() {
+            if character.is_numeric() {
+                calibration += character.to_digit(10).unwrap();
+                break;
+            }
+        }
+        sum += calibration
     }
-    Some(highest_cal)
+    Some(sum)
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
-    let v: Vec<&str> = _input.split('\n').collect();
-    let mut total_calories = 0;
-    let mut highest_calories = vec![0,0,0];
-    for element in v {
-        if element.parse::<u32>().is_ok(){
-            total_calories = total_calories + element.parse::<u32>().unwrap();
-        } else {
-            if highest_calories[0] < total_calories {
-                highest_calories[2] = highest_calories[1];
-                highest_calories[1] = highest_calories[0];
-                highest_calories[0] = total_calories;
-            } else if highest_calories[1] < total_calories {
-                highest_calories[2] = highest_calories[1];
-                highest_calories[1] = total_calories;
-            } else if highest_calories[2] < total_calories {
-                highest_calories[2] = total_calories;
-            }
-            total_calories = 0
-        }
-    }
-    let mut sum_highest_calories = 0;
-    for each in highest_calories {
-        sum_highest_calories += each
-    }
-    Some(sum_highest_calories)
-}
-
-pub fn first_function() -> i32 {
-    return 10
+    let mut numbers_galore = String::from(_input);
+    numbers_galore = numbers_galore.replace("one", "o1e");
+    numbers_galore = numbers_galore.replace("two", "t2o");
+    numbers_galore = numbers_galore.replace("three", "t3e");
+    numbers_galore = numbers_galore.replace("four", "f4r");
+    numbers_galore = numbers_galore.replace("five", "f5e");
+    numbers_galore = numbers_galore.replace("six", "s6x");
+    numbers_galore = numbers_galore.replace("seven", "s7n");
+    numbers_galore = numbers_galore.replace("eight", "e8t");
+    numbers_galore = numbers_galore.replace("nine", "n9e");
+    numbers_galore = numbers_galore.replace("zero", "z0o");
+    part_one(&numbers_galore)
 }
 
 #[cfg(test)]
@@ -54,28 +42,16 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
+#[ignore]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(24000));
+        assert_eq!(result, Some(209));
     }
 
     #[test]
-    #[ignore]
+#[ignore]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(41000));
-    }
-
-    #[test]
-    fn test_print_hello_world () {
-        println!("hello world");
-        assert_eq!(1, 1);
-    }
-
-    #[test]
-    fn test_return_integer () {
-        let expected = 10;
-        assert_eq!(first_function(), expected);
+        assert_eq!(result, Some(281));
     }
 }
